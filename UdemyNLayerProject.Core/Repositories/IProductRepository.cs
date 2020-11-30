f1835f9255a1f17000601e046ba0fe6bd07fb663
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-11-30 22:36:54 +0300

    Core Katman - IProductRepository Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    interface IProductRepository
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
