ac0e6d765ff29e4b3f291c0a83cfd8445b65377b
yenilikci <melihcelik017@outlook.com>
2020-11-30 11:31:13 +0300

    Core Katman - ICategoryRepository Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    interface ICategoryRepository:IRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
