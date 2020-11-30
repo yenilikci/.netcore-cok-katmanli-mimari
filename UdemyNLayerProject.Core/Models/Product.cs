382d55e3a6589089b8f388525ea219b64e9f1783
yenilikci <melihcelik017@outlook.com>
2020-11-30 10:48:50 +0300

    Core Katman - Product ve Category Modelleri Oluşturuldu

---
+++ UdemyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public int InnerBarcode { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
