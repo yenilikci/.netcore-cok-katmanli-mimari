8b667190dc259e665c2775fd9d9778f07d46c2f2
yenilikci <melihcelik017@outlook.com>
2020-11-30 11:12:07 +0300

    Core Katman - IRepository Generic Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    interface IRepository<TEntity> where TEntity:class
+    {
+
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entity);
+        TEntity Update(TEntity entity);
+
+    }   
+}
