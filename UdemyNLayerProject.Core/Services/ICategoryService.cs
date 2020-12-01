a568ed3d936b35a74b764be1377a4b9c41319ea7
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-01 15:13:13 +0300

    Core Katman - ICategoryService Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Core.Services
+{
+    interface ICategoryService:IService<Category>
+    {
+       Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+        //Category özgü metotlar burada tanımlanabilir
+    }
+}
