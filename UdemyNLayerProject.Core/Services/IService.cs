6ec66481faaa939f1b843d07fe4bc478810eb1df
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-01 15:02:36 +0300

    Core Katman - IService Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/Services/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Core.Services
+{
+    interface IService<TEntity> where TEntity:class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entity);
+        TEntity Update(TEntity entity);
+
+    }
+}
