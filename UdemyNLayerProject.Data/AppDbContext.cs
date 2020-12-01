c6de479a11936f217e6633c95756c1e35b1c229b
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-01 15:45:26 +0300

    Data Katman - DbContext Sınıfı Oluşturuldu

---
+++ UdemyNLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Data
+{
+    class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
