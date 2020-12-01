0f793d2c8d3f784ed43907119f543419ab2f46bf
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-01 15:24:53 +0300

    Core Katman - IUnitOfWork Interface Oluşturuldu

---
+++ UdemyNLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+        void Commit();
+    }
+}
