eaaaedb59c2324696e24391795d95bb0c017ff6a
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-02 16:23:35 +0300

    Data Katman - Repository Oluşturuldu

--- UdemyNLayerProject.Core/Repositories/IRepository.cs
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -11,12 +11,12 @@ namespace UdemyNLayerProject.Core.Repositories
 
         Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
         Task AddRangeAsync(IEnumerable<TEntity> entities);
         void Remove(TEntity entity);
-        void RemoveRange(IEnumerable<TEntity> entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
         TEntity Update(TEntity entity);
 
     }   
