d654e67e35d9ec4cfd27965199fe291683573877
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-02 15:30:01 +0300

    Data Katman - CategorySeed Oluşturuldu

---
+++ UdemyNLayerProject.Data/Seeds/CategorySeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Data.Seeds
+{
+    class CategorySeed:IEntityTypeConfiguration<Category>
+    {
+        private readonly int[] _ids;
+        public CategorySeed(int[] ids)
+        {
+            _ids = ids;
+        }
+
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = _ids[0], Name = "Kalemler" },
+                new Category { Id = _ids[1], Name = "Defterler" }
+                );
+        }
+    }
+}
