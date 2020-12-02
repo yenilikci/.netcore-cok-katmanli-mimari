1abb551257d3cd0115ca8c5e64e50f4bbb9533ba
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-02 14:30:15 +0300

    Data Katman - CategoryConfiguration Oluşturuldu

---
+++ UdemyNLayerProject.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Data.Configurations
+{
+    class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //primary key Id olsun
+            builder.HasKey(x => x.Id);
+            //Id değeri 1'er artacak
+            builder.Property(x => x.Id).UseIdentityColumn();
+            //Name zorunlu olsun, 50 karakter uzunluğunda
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            //Tablo adı
+            builder.ToTable("Categories"); //.ToTable("tabloAdi","semaAdi");
+
+        }
+    }
+}
