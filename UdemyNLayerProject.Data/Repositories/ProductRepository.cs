7489f148368a0fc941cb9743505b553236b9466c
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-02 16:35:08 +0300

    Data Katman - ProductRepository Oluşturuldu

---
+++ UdemyNLayerProject.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+    class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private AppDbContext appDbContext { get => _context as AppDbContext; }
+
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == productId);
+        }
+    }
+}
