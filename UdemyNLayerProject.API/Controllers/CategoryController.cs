45cfff06d949e77ac97153305799acce470f1a4a
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-04-17 02:13:28 +0300

    CategoryController Oluşturulması

---
+++ UdemyNLayerProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+    }
+}
