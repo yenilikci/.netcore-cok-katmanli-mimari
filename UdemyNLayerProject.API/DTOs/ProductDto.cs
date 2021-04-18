29f840fd4c5ee3e95ddd8452cac09ab6092a33dc
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-04-18 06:12:57 +0300

    ProductDto.cs Classının Oluşturulması

---
+++ UdemyNLayerProject.API/DTOs/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.API.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
