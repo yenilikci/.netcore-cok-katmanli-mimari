7f7e2430815bbf2e0473e63b07d730bd45b57099
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-04-18 05:43:49 +0300

    CategoryController Save Methodunun Oluşturulması

--- UdemyNLayerProject.API/Controllers/CategoryController.cs
+++ UdemyNLayerProject.API/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UdemyNLayerProject.API.DTOs;
+using UdemyNLayerProject.Core.Models;
 using UdemyNLayerProject.Core.Services;
 
 namespace UdemyNLayerProject.API.Controllers
@@ -36,6 +37,13 @@ namespace UdemyNLayerProject.API.Controllers
         {
             var category = await _categoryService.GetByIdAsync(id);
             return Ok(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+            var newCategory = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            return Created(string.Empty,_mapper.Map<CategoryDto>(newCategory));
         }
 
     }
