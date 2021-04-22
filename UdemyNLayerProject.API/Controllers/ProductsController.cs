374058a826297eb89b218256fae5f218e04dc74c
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-04-22 07:57:29 +0300

    ProductsController GetAll Methodunun Oluşturulması

---
+++ UdemyNLayerProject.API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.API.DTOs;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<ProductDto>>(products));
+        }
+
+
+    }
+
+}
